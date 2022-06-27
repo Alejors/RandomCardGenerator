fa93e4cf06a926dac326d2a5418518bc52ac679c
alejoatria <alejoatria@gmail.com>
2022-06-27 11:52:44 -0400

    Se agrega funcionalidad de generar nueva carta aleatoria. Carta se muestra bajo la anterior. Falta mejorar display

--- src/app.js
+++ src/app.js
@@ -1,34 +1,41 @@
-window.onload = function () {
-  //FUNCION PARA NUMERO
-  var valor = document.querySelector('.center');
-  var number = Math.floor(Math.random() * 13 + 1);
+var contador = 0;
+
+function getnumber() {
+  let number = Math.floor(Math.random() * 13 + 1);
   if (number == 13) {
     let K = document.createTextNode('K');
-    valor.appendChild(K);
+    return K;
   }
   else if (number == 12) {
     let Q = document.createTextNode('Q');
-    valor.appendChild(Q);
+    return Q;
   }
   else if (number == 11) {
     let J = document.createTextNode('J');
-    valor.appendChild(J);
+    return J;
   }
   else if (number == 1) {
     let A = document.createTextNode('A');
-    valor.appendChild(A);
+    return A;
   }
   else {
     let text = document.createTextNode(`${number}`);
-    valor.appendChild(text);
+    return text;
   }
+}
+
+window.onload = function () {
+  console.log(contador);
+  //FUNCION PARA NUMERO
+  let valor = document.querySelector('#center');
+  valor.appendChild(getnumber());
+
   //FUNCION PARA PINTA
-  var pinta = document.getElementsByClassName('suit');
-  console.log(pinta);
+  var pinta = document.querySelectorAll('.suit');
   let selectorpinta = Math.floor(Math.random() * 4);
-  console.log(selectorpinta);
+
   if (selectorpinta == 0 || selectorpinta == 1) {
-    document.querySelector('.center').style.color = 'red';
+    document.querySelector('#center').style.color = 'red';
     if (selectorpinta == 0) {
       for (var i = 0; i < pinta.length; i++) {
         pinta[i].innerHTML = '♥';
@@ -52,4 +59,75 @@ window.onload = function () {
       pinta[i].innerHTML = '♣';
     }
   };
+
+  //BOTON NUEVA CARTA
+  var boton = document.getElementById("boton");
+  boton.addEventListener('click', (e) => {
+    contador++;
+    console.log(`contador:${contador}`);
+
+    var cuerpo = document.querySelector('body');
+    let nuevacarta = document.createElement("div");
+    nuevacarta.classList.add("carta");
+    let top = document.createElement("div");
+    top.classList.add(`suit${contador}`);
+    top.setAttribute("id", "top")
+    nuevacarta.appendChild(top);
+    let mid = document.createElement("div");
+    mid.setAttribute("id", `center${contador}`);
+    nuevacarta.appendChild(mid);
+    let btm = document.createElement("div");
+    btm.classList.add(`suit${contador}`);
+    btm.setAttribute("id", "bottom")
+    nuevacarta.appendChild(btm);
+    cuerpo.appendChild(nuevacarta);
+
+    //DAR NUMERO A LA CARTA NUEVA
+    var valor1 = document.getElementById(`center${contador}`);
+    valor1.appendChild(getnumber());
+
+    //FUNCION PARA PINTA
+    var pinta = document.querySelectorAll(`.suit${contador}`);
+    let selectorpinta = Math.floor(Math.random() * 4);
+    if (selectorpinta == 0 || selectorpinta == 1) {
+      document.querySelector(`#center${contador}`).style.color = 'red';
+      if (selectorpinta == 0) {
+        for (var i = 0; i < pinta.length; i++) {
+          pinta[i].innerHTML = '♥';
+          pinta[i].style.color = "red";
+        }
+      }
+      else {
+        for (var i = 0; i < pinta.length; i++) {
+          pinta[i].innerHTML = '♦';
+          pinta[i].style.color = 'red';
+        }
+      }
+    }
+    else if (selectorpinta == 2) {
+      for (var i = 0; i < pinta.length; i++) {
+        pinta[i].innerHTML = '♠';
+      }
+    }
+    else {
+      for (var i = 0; i < pinta.length; i++) {
+        pinta[i].innerHTML = '♣';
+      }
+    };
+  });
+
+  //CAMBIO DE MEDIDAS CARTA
+  var ancho = document.getElementById("width");
+  var carta = document.querySelector(".carta");
+  ancho.addEventListener("change", (e) => {
+    if (ancho.value != '') {
+      parseInt(ancho);
+      carta.style.width = ancho + 'px';
+      ancho.value = "";
+    }
+  });
+
+
+
+
 }
